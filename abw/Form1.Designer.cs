700a19472d2ad7ecbe789f426062d4384cceeba9
dima-kopat <dima.kopat@gmail.com>
2017-04-05 12:01:50 +0300

    Добавил проект abw

---
+++ abw/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace abw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Mark = new System.Windows.Forms.ComboBox();
+            this.Model = new System.Windows.Forms.ComboBox();
+            this.Model2 = new System.Windows.Forms.ComboBox();
+            this.Mark2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Mark
+            // 
+            this.Mark.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Mark.FormattingEnabled = true;
+            this.Mark.Location = new System.Drawing.Point(13, 13);
+            this.Mark.Name = "Mark";
+            this.Mark.Size = new System.Drawing.Size(151, 21);
+            this.Mark.TabIndex = 0;
+            this.Mark.SelectedIndexChanged += new System.EventHandler(this.Mark_SelectedIndexChanged);
+            // 
+            // Model
+            // 
+            this.Model.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Model.FormattingEnabled = true;
+            this.Model.Location = new System.Drawing.Point(196, 13);
+            this.Model.Name = "Model";
+            this.Model.Size = new System.Drawing.Size(151, 21);
+            this.Model.TabIndex = 1;
+            // 
+            // Model2
+            // 
+            this.Model2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Model2.FormattingEnabled = true;
+            this.Model2.Location = new System.Drawing.Point(196, 134);
+            this.Model2.Name = "Model2";
+            this.Model2.Size = new System.Drawing.Size(151, 21);
+            this.Model2.TabIndex = 3;
+            // 
+            // Mark2
+            // 
+            this.Mark2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Mark2.FormattingEnabled = true;
+            this.Mark2.Location = new System.Drawing.Point(13, 134);
+            this.Mark2.Name = "Mark2";
+            this.Mark2.Size = new System.Drawing.Size(151, 21);
+            this.Mark2.TabIndex = 2;
+            this.Mark2.SelectedIndexChanged += new System.EventHandler(this.Mark2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 453);
+            this.Controls.Add(this.Model2);
+            this.Controls.Add(this.Mark2);
+            this.Controls.Add(this.Model);
+            this.Controls.Add(this.Mark);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Mark;
+        private System.Windows.Forms.ComboBox Model;
+        private System.Windows.Forms.ComboBox Model2;
+        private System.Windows.Forms.ComboBox Mark2;
+    }
+}
+
---
+++ abw/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace abw
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument document;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Mark_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Model.Items.Clear();
+            switch(Mark.SelectedIndex)
+            {
+                case 0:
+                    Model.Items.Add("A3");
+                    Model.Items.Add("A4");
+                    Model.Items.Add("A6");
+                    Model.Items.Add("A7");
+                    Model.Items.Add("A8");
+                    break;
+                case 1: 
+                    Model.Items.Add("3 series");
+                    Model.Items.Add("5 series");
+                    Model.Items.Add("7 series");
+                    Model.Items.Add("X3");
+                    Model.Items.Add("X5");
+                    break;
+                case 2:
+                    Model.Items.Add("300C");
+                    Model.Items.Add("Crossfire");
+                    Model.Items.Add("Intrepid");
+                    Model.Items.Add("Neon");
+                    Model.Items.Add("Sebring");
+                    break;
+                case 3:
+                    Model.Items.Add("Caliber");
+                    Model.Items.Add("Dakota");
+                    Model.Items.Add("Magnum");
+                    Model.Items.Add("Ram");
+                    Model.Items.Add("Viper");
+                    break;
+                case 4:
+                    Model.Items.Add("500");
+                    Model.Items.Add("Albea");
+                    Model.Items.Add("Coupe");
+                    Model.Items.Add("Stilo");
+                    Model.Items.Add("Palio");
+                    break;                    
+            }
+            Model.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Mark.Items.Add("Audi");
+            Mark.Items.Add("BMW");
+            Mark.Items.Add("Chrysler");
+            Mark.Items.Add("Dodge");
+            Mark.Items.Add("Fiat");
+            Mark.SelectedIndex = 0;
+
+            document = new XmlDocument();
+            document.Load("../../libCars.xml");
+
+            XmlNodeList xmlNodeList1 = document.SelectNodes("/cars/Mark/name");
+            foreach(XmlNode node in xmlNodeList1)
+            {
+                Mark2.Items.Add(node.InnerText);
+            }
+            Mark2.SelectedIndex = 0;
+        }
+
+        private void Mark2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Model2.Items.Clear();
+            string path = "/cars/Mark[name='" + Mark2.SelectedItem.ToString() + "']/models/model";
+            XmlNodeList xmlNodeList2 = document.SelectNodes(path);
+            foreach (XmlNode node in xmlNodeList2)
+            {
+               Model2.Items.Add(node.InnerText);
+            }
+            Model2.SelectedIndex = 0;
+        }
+    }
+}
