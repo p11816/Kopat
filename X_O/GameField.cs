d7b75b701ad197a45e2dfbeb6ce2c90bba53616c
dima-kopat <dima.kopat@gmail.com>
2017-04-11 11:52:13 +0300

    add X-O

---
+++ X_O/GameField.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X_O
+{
+    public partial class GameField : Form, IControler, Iview
+    {
+        private GameModel model = new GameModel();
+        private Button[,] field;
+        // Dictionary<GameModel .State>
+        public GameField()
+        {
+            InitializeComponent();
+           // model.UpdateView += 
+            field = new Button[3,3];
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); i++)
+                {
+                    Button b = new Button();
+                    b.Left = i * 50 + 10;
+                    b.Top = j * 50 + 10;
+                    b.Size = new Size(40, 40);
+                    b.Tag = new Point(i, j);
+                    b.Click += GameField_Click;
+                    //добавляем кнопку на форму
+                    this.Controls.Add(b);
+                    field[i.j] = b;
+                }
+            }
+        }
+        //обработчик для кнопки
+        private void GameField_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            //координаты кнопки
+            Point p = (Point)b.Tag;
+
+        }
+
+        void Iview.UpdateView(GameModel model)
+        {
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field.GetLength(1); i++)
+                {
+                    
+                   // field[i,j].Text = 
+                }
+            }
+        }
+    }
+}
---
+++ X_O/GameModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X_O
+{
+    class GameModel
+    {
+        enum State
+        {
+            X,
+            O,
+            none
+        }
+
+        public GameModel()
+        {
+            Field = new State[3, 3]; 
+            for (int i = 0; i < Field.GetLength(0); i++)
+            {
+                for (int j = 0; j < Field.GetLength(1); i++)
+                {
+                    Field[i, j] = State.none;
+                }
+            }
+            CountStep = 0;
+            Winner = State.none;
+            GameOver = false;
+            CurrentMove = State.X;
+        }
+        public State[,] Field { get; private set; }
+        public int CountStep { get; private set; }
+        public State Winner  { get; private set; }
+        public bool GameOver { get; private set; }
+        public State CurrentMove { get; private set; }
+        public delegate void UpdateViewDelegate();
+        public event UpdateViewDelegate UpdateView;
+        public void MakeMove(int i, int j, State side)
+        {
+            if(GameOver)
+            {
+                throw new Exception("Game is already over!");
+            }
+            if (i < 0 || i > 2 || j < 0 || j > 2)
+            {
+                throw new IndexOutOfRangeException("Index out of view!");
+            }
+            if(side != CurrentMove)
+            {
+                throw new Exception("It is not your turn!");
+            }
+            if(Field[i,j] != State.none)
+            {
+                throw new Exception("Not empty field!");
+            }
+
+            Field[i, j] = side;
+            CountStep++;
+            CheckForGameOver();
+            CurrentMove = side == State.O ? State.X : State.O;
+        }
+        private void CheckForGameOver()
+        {
+            if(CountStep < 5)
+            {
+                return;
+            }
+            //проверку на выстроенную линию и т.д.
+ 	        throw new NotImplementedException();
+        }
+    }
+}
